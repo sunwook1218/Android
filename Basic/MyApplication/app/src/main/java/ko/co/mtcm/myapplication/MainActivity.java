28ff9b7d492e87f38f4abb903cebc8dbc58be310
sunwook1218 <sunwook1218@naver.com>
2021-02-25 13:24:02 +0900

    Intent 이용하여 브라우저 호출, 전화기능 호출

--- Basic/MyApplication/app/src/main/java/ko/co/mtcm/myapplication/MainActivity.java
+++ Basic/MyApplication/app/src/main/java/ko/co/mtcm/myapplication/MainActivity.java
@@ -1,14 +1,38 @@
 package ko.co.mtcm.myapplication;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import ko.co.mtcm.myapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding bd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        bd = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(bd.getRoot());
     }
+
+    public void func1(View view) {
+        Toast.makeText(this, "button click", Toast.LENGTH_SHORT).show();
+    }
+
+    public void func2(View view) {
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+        startActivity(myIntent);
+    }
+
+    public void func3(View view) {
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:" + bd.numEt.getText()));
+        startActivity(myIntent);
+    }
+
 }
